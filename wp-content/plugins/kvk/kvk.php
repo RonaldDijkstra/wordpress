2c79fbfdd5b86a998dd8e0ddeb03681281b31178
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2023-04-24 10:14:00 +0200

    Setup plugin

---
+++ wp-content/plugins/kvk/kvk.php
@@ -0,0 +1,9 @@
+/**
+ * Plugin Name: KVK
+ * Plugin URI: 
+ * Description: Test the KVK API
+ * Version: Test
+ * Author: Ronald Dijkstra
+ * Author URI: https://github.com/RonaldDijkstra
+ * License: 
+ */
