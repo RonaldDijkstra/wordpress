a747ba167b3e556d79e3a8398203860d8aab22e6
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2023-04-24 11:06:10 +0200

    Retrieve them data

--- wp-content/plugins/kvk/kvk.php
+++ wp-content/plugins/kvk/kvk.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Plugin Name: KVK
  * Plugin URI: 
@@ -7,3 +8,43 @@
  * Author URI: https://github.com/RonaldDijkstra
  * License: 
  */
+
+ 
+ function get_kvk_data()
+ {
+    // $apikey = 'l7xx1f2691f2520d487b902f4e0b57a0b197'; // TODO: Move to env var
+    $rest_api_url = 'https://api.kvk.nl/test/api/v1/zoeken?apikey=l7xx1f2691f2520d487b902f4e0b57a0b197';
+
+    $curl = curl_init();
+
+    // Set the URL and other curl options
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $rest_api_url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_SSL_VERIFYHOST => false, // TODO: Fix this stupid danger later!!
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+    ));
+
+    // Send the request and get the response
+    $response = curl_exec($curl);
+    $error = curl_error($curl);
+
+    // Close the curl session
+    curl_close($curl);
+
+    // If there was an error, return it
+    if ($error) {
+        return "Error: " . $error;
+    }
+
+    // Decode the JSON response and return it as an associative array
+    $data = json_decode($response, true);
+    $results = $data['resultaten'];
+
+    return $results;
+ }
